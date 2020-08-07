5a4aec9ee8619fdd6d704b8ac8ce7b7eeb676918
Derek <exiaowalkman@gmail.com>
2020-08-07 14:20:39 -0400

    0808

--- Search for a Range.js
+++ Search for a Range.js
@@ -22,30 +22,44 @@ var searchRange = function(nums, target) {
     let result = [-1,-1];
     let pos1 = 0;
     let pos2 = nums.length-1;
+    result[0] = go(nums, target, pos1, pos2, 0);
+    result[1] = go(nums, target, pos1, pos2, 1);
+    return result;
+};
+
+function go(nums, target, pos1, pos2, mode){
     while(true){
+        // console.log(pos1,pos2);
         let middlePos = pos1 + Math.floor((pos2-pos1)/2);
+        // console.log(middlePos+'---------');
         if(nums[middlePos] == target){
-            if((middlePos == 0 || nums[middlePos-1] != target)){
-                result[0] = middlePos;
-                break;
+            if(mode == 0 && (middlePos == 0 || nums[middlePos-1] != target)){
+                return middlePos;
             }
-            // if((middlePos == nums.length-1 || nums[middlePos+1] != target)){
-            //     result[1] = middlePos;
-            // }
-            // break;
+            if(mode == 1 && (middlePos == nums.length-1 || nums[middlePos+1] != target)){
+                return middlePos;
+            }
+            if(mode == 0){
+                pos2 = middlePos - 1;
+                continue;
+            }
+            if(mode == 1){
+                pos1 = middlePos + 1;
+                continue;
+            }
         }
         if(nums[middlePos] < target){
             pos1 = middlePos + 1;
         }else{
             pos2 = middlePos - 1;
         }
-        if(pos1 == pos2 || pos1+1 == pos2){
-            break;
+        if(pos1 > pos2){
+            return -1;
         }
     }
-    return result;
-};
+}
 
-// NOT finished yet--------------
 console.log(searchRange([5,7,7,8,8,10], 8));
 console.log(searchRange([5,7,7,8,8,10], 6));
+console.log(searchRange([5,6], 6));
+console.log(searchRange([], 6));
