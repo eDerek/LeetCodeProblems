df43c1146cfe898768dc4b34bf4a933832583b4f
Derek <exiaowalkman@gmail.com>
2020-09-19 02:21:11 -0400

    basic calculator (with regEx)

--- Basic Calculator.py
+++ Basic Calculator.py
@@ -18,10 +18,47 @@
 # You may assume that the given expression is always valid.
 # Do not use the eval built-in library function.
 
+import re
+
 class Solution:
+    flag = False
     def calculate(self, s: str) -> int:
-        return 0
+        if not self.flag:
+            s = s.replace(' ','')
+            self.flat = True
+        subEs = re.findall(r'\([\d+-]+\)', s)
+        if len(subEs) == 0:
+            return self.simpleExpress(s)
+        for sub in subEs:
+            valStr = str(self.simpleExpress(sub.replace('(','').replace(')','')))
+            s = s.replace(sub, valStr, 1).replace('+-','-', 1).replace('--','+',1)
+        # print(s)
+        return self.calculate(s)
+
+    def simpleExpress(self, s:str) -> int:
+        # print(s)
+        sArray = list(s)
+        result = 0
+        currOpt = 1
+        currVal = 0
+        for c in sArray:
+            if c=='+':
+                # print(currVal)
+                result += currOpt*currVal
+                currVal = 0
+                currOpt = 1
+            elif c=='-':
+                result += currOpt*currVal
+                currVal = 0
+                currOpt = -1
+            else:
+                currVal = currVal*10+int(c)
+        result += currOpt*currVal
+        # print(result)
+        return result
 
 print(Solution().calculate('1 + 1'))
 print(Solution().calculate('2-1 + 2'))
+print(Solution().calculate('4+5+2'))
 print(Solution().calculate('(1+(4+5+2)-3)+(6+8)'))
+print(Solution().calculate('2-(5-6)'))
